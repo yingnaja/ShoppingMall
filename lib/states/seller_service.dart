9fa593657f0a10b0f186fa4717d12a06c75e01e4
yingnaja <sinyapongs.com>
2022-02-25 15:29:20 +0700

    EP6 Upload

---
+++ lib/states/seller_service.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SellerService extends StatefulWidget {
+  const SellerService({Key? key}) : super(key: key);
+
+  @override
+  _SellerServiceState createState() => _SellerServiceState();
+}
+
+class _SellerServiceState extends State<SellerService> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/utility/my_constant.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MyConstant {
+
+  // General
+  static String appName = 'Shopping Mall';
+
+  // Route
+  static String routeAuthen = '/authen';
+  static String routeCreateAccount = '/createAccount';
+  static String routeBuyerService = '/buyerService';
+  static String routeSellerService = '/sellerService';
+  static String routeRiderService = '/riderService';
+  
+}
+
