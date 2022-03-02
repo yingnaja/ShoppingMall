a94882410e73df4386dcacc6f281b0efe5572b56
yingnaja <sinyapongs.com>
2022-03-02 13:47:49 +0700

    Upload EP10 authen.dart

--- lib/utility/my_constant.dart
+++ lib/utility/my_constant.dart
@@ -1,7 +1,8 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class MyConstant {
-
   // General
   static String appName = 'Shopping Mall';
 
@@ -11,6 +12,30 @@ class MyConstant {
   static String routeBuyerService = '/buyerService';
   static String routeSellerService = '/sellerService';
   static String routeRiderService = '/riderService';
-  
-}
 
+  // Images
+  static String image1 = 'images/image1.png';
+  static String image2 = 'images/image2.png';
+  static String image3 = 'images/image3.png';
+  static String image4 = 'images/image4.png';
+
+  // Color
+  static Color primary = Color(0xff87861d);
+  static Color dark = Color(0xff575900);
+  static Color light = Color(0xffb9b64e);
+
+  // Style
+  TextStyle h1Style() =>
+      TextStyle(fontSize: 24, color: dark, fontWeight: FontWeight.bold);
+  TextStyle h2Style() =>
+      TextStyle(fontSize: 18, color: dark, fontWeight: FontWeight.w700);
+  TextStyle h3Style() =>
+      TextStyle(fontSize: 14, color: dark, fontWeight: FontWeight.normal);
+
+  ButtonStyle myButtonStyle() => ElevatedButton.styleFrom(
+        primary: MyConstant.primary,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+        ),
+      );
+}
